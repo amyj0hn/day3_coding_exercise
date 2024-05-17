e3defd2bf4fea730e0b67b05fa34de01a7cf6177
amyj0hn <amyj62450@gmail.com>
2024-05-17 14:46:44 +0200

    added data types

---
+++ script/code.js
@@ -0,0 +1,21 @@
+let cat = "Jingles" //string
+console.log(cat)
+console.log(typeof cat)
+
+
+cat = 1 //number
+console.log(cat)
+console.log(typeof cat)
+
+
+let x = 20>100 //boolean
+console.log(x)
+console.log(typeof x)
+
+let age; //undefined
+console.log(age)
+console.log(typeof age)
+
+let number = null
+console.log(number)
+console.log(typeof null)
